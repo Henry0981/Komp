e082c5accd649c0ad60addb2a31984e7675ad003
Henry Gillinger <henry.gillinger@ga.ntig.se>
2023-05-29 21:38:46 +0200

    first commit

---
+++ Komp/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Hej vad heter du? ,skriv ditt namn här:");
+
+// 
+
+string n = name();
+int s = 10;
+
+Console.WriteLine($"ok vad bra Hej, {n}");
+
+// få ut nament ur metoden!!!
+// en output värde
+
+Console.ReadLine();
+
+
+
+static string name()
+{
+
+    string name = Console.ReadLine();
+    int shhh;
+    bool hsgdhs =  int.TryParse(name,out shhh);
+    if (shhh == 10) {
+        Console.WriteLine("fel");
+    }
+   return name;
+
+}
